ff13f1e30e271c4e15b1f1126762b3eaf72bf8a1
joydip mondal <joydip.mondal@gmail.com>
2024-05-01 15:05:57 +0530

    react code uploaded

---
+++ codes/day-13/first-react-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  const message = "welcome to React JS"
+  const design = (
+    <div className="App">
+      {message}
+    </div>
+  );
+  return design
+}
+
+export default App;
