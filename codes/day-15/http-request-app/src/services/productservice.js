540da7b48e4e5e4b95afa08283af841d22b270e3
Joydip <joydip.mondal@gmail.com>
2024-05-03 15:43:01 +0530

    Update productservice.js

--- codes/day-15/http-request-app/src/services/productservice.js
+++ codes/day-15/http-request-app/src/services/productservice.js
@@ -1,32 +1,37 @@
+import Axios from 'axios'
 //1. fetch() API of browser (DOM library)
 //2. axios package: 3rd party package
 
 //call RESTful API to get all records of employees
 //url: http://localhost:8001/employees/sort/{sortchoice} (GET)
 
-const baseUrl = 'http://localhost:8001/employees/'
-function getEmployees() {
+const baseUrl = 'http://localhost:8001/employees'
 
+function getEmployees(sortChoice) {
+    //get() method sends the request to RESTful API asynchronously and the method returns a Promise object of type AxiosResponse (Promise<AxiosResponse>)
+    return Axios.get(`${baseUrl}/sort/${sortChoice}`)
 }
+
+
 //call RESTful API to get a single record of an employee
 //url: http://localhost:8001/employees/view/{eid} (GET)
 function getEmployee(id) {
-
+    return Axios.get(`${baseUrl}/view/${id}`)
 }
 //call RESTful API to pass a newly created employee data
 //so that it can saved in the database table
 //url: http://localhost:8001/employees/add (POST)
 function addEmployee(empObject) {
-
+    return Axios.post(`${baseUrl}/add`, empObject)
 }
 
 //call RESTful API to pass a newly updated an existing employee data so that it can updated also in the database table
 //url: http://localhost:8001/employees/update/{eid} (PUT)
 function updateEmployee(id, empObject) {
-
+    return Axios.put(`${baseUrl}/update/${id}`, empObject)
 }
 //call RESTful API to delete an employee record from database tabke
 //url: http://localhost:8001/employees/delete/{eid} (DELETE)
 function deleteEmployee(id) {
-
+    return Axios.delete(`${baseUrl}/delete/${id}`)
 }
